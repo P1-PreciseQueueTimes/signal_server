5835f49456727529ad7b397db6365ab18c842b03
Martin Nammat <martinjens986@gmail.com>
2024-11-25 13:12:59 +0100

    add sync

--- main.py
+++ main.py
@@ -5,14 +5,24 @@ app = Flask(__name__)
 
 sent_time = 0
 
+receivers = {}
+
+test = [1,2,3,4,5]
+
+sum(test)
+
 @app.route("/")
 def hello_world():
     return "It is up :)" 
 
-@app.route("/get/testing/time",methods=["GET"])
-def get_time(): 
+@app.route("/get/calibration/diff_time/<host_name>",methods=["GET"])
+def get_time(host_name): 
 
-    return str(round(time.time()))
+    difference_in_times = receivers[host_name]
+
+    average_difference = sum(difference_in_times)/len(difference_in_times)
+
+    return str(average_difference) 
 
 @app.route("/post/testing/sender",methods=["POST"])
 def post_sender(): 
@@ -31,15 +41,26 @@ def post_sender():
 
 @app.route("/post/testing/receiver",methods=["POST"])
 def post_receiver():
-    global sent_time
+    global sent_time, receivers
     request_data = request.get_json()
 
     host_name = request_data["host_name"]
 
     pie_time = request_data["internal_time"]
-    diff_time = (pie_time - sent_time) / 1000000
+
+    signal_strength= request_data["signal_strength"]
+
+    diff_time_ns = pie_time - sent_time  
+
+    diff_time_ms = diff_time_ns / 1000000.0
+
+    if host_name in receivers.keys():
+        receivers[host_name].append(diff_time_ns)
+    else:
+        receivers[host_name] = [diff_time_ns]
+
     print()
-    print("Host Name: {}\nPie Time: {}\nDiff Time ms: {}\n".format(host_name,pie_time,diff_time))
+    print("Host Name: {}\nPie Time: {}\nDiff Time ms: {}\nSignal Strength db:{}\n".format(host_name,pie_time,diff_time_ms,signal_strength))
 
     return "" 
 
